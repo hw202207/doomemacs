1e63c9fd12156a2704989675cc19bab3a9f25a70
Haisheng W - M <haisheng@mercury.com>
2023-12-22 15:33:00 -0800

    bump forge

--- modules/tools/magit/packages.el
+++ modules/tools/magit/packages.el
@@ -3,12 +3,14 @@
 
 (when (package! magit :pin "48818355728c48d986d74dde8b1e9fba25f0fd53")
   (when (modulep! +forge)
-    (package! forge :pin "ba35ffc9bafc6457cc95633904e53e34e544543f")
+    (package! forge :pin "a6e0a634f87154188a0fe7f1ec8bc81dd65402a4")
     (package! code-review
-      :recipe (:files ("graphql" "code-review*.el"))
-      :pin "26f426e99221a1f9356aabf874513e9105b68140")
+      :recipe (:host github
+               :repo "doomelpa/code-review/"
+               :files ("graphql" "code-review*.el"))
+      :pin "c34ff1ee64c7ecb654635bee4e2c147b10c66297"))
     ; HACK closql c3b34a6ec438 breaks code-review wandersoncferreira/code-review#245,
     ; and the current forge commit (but forge does have an upstream fix),
     ; pinned as a temporary measure to prevent user breakages
-    (package! closql :pin "0a7226331ff1f96142199915c0ac7940bac4afdd"))
+    ;; (package! closql :pin "0a7226331ff1f96142199915c0ac7940bac4afdd"))
   (package! magit-todos :pin "cadf29d1cc410c71a0020c7f83999d9f61721b90"))
